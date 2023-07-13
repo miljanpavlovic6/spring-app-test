de9c58d49a411d16b8f51962d975db601997f61f
Miljan Pavlovic <miljan.pavlovic@ibm.com>
2023-07-13 10:35:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Spring app for testing on Linux host, docker or Kubernetes
+
+## Deploying on Linux host
+
+## Deploying on Docker
+
+### Create the local image from Dockerfile
+-Dockerfile
+```
+FROM openjdk:8-jdk-alpine
+EXPOSE 8090
+ARG JAR_FILE=app-test-1.jar
+ADD ${JAR_FILE} app-test-1.jar
+ENTRYPOINT ["java","-jar","/app-test-1.jar"]
+```
+Execute and test locally
+```
+docker build --tag=spring-app-test:latest .
+docker run -p 8090:8090 spring-app-test:latest
+```
+-and push to docker hub
+```
+docker login -u miljanpavlovic -p <password>
+docker tag spring-app-test:latest miljanpavlovic/spring-app-test:latest
+docker push miljanpavlovic/spring-app-test:latest
+```
+
+
+## Deploying on Kubernetes
---
+++ src/main/java/org/selflearning/demosimpleapi/DemoSimpleApiApplication.java
@@ -0,0 +1,30 @@
+package org.selflearning.demosimpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@RestController
+@SpringBootApplication
+public class DemoSimpleApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSimpleApiApplication.class, args);
+    }
+
+    @GetMapping("/")
+    public String greetMe(){
+
+        try {
+            return "Hostname : " + InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return "Unkown";
+    }
+
+}
